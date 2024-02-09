4e2ea0a862c539422148b105a3d0b63e708579dd
Benjamin-Harris1 <beha1503@gmail.com>
2024-02-09 16:28:40 +0100

    Made code more generic and simple to understand

--- src/edu/hogwarts/application/UserInterFace.java
+++ src/edu/hogwarts/application/UserInterFace.java
@@ -5,7 +5,6 @@ import edu.hogwarts.controller.TeacherController;
 import edu.hogwarts.data.HogwartsStudent;
 import edu.hogwarts.data.HogwartsTeacher;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -43,57 +42,6 @@ public class UserInterFace {
         }
     }
 
-    public Comparator<HogwartsStudent> chooseStudentComparator(String sortField, String sortOrder) {
-        Comparator<HogwartsStudent> comparator = (a, b) -> 0; // Ingen sortering
-
-        switch(sortField.toLowerCase()) {
-            case "firstname":
-                comparator = (a, b) -> a.getFirstName().compareTo(b.getFirstName());
-                break;
-            case "lastname":
-                comparator = (a, b) -> a.getLastName().compareTo(b.getLastName());
-                break;
-            case "house":
-                comparator = (a, b) -> a.getHouse().getName().compareTo(b.getHouse().getName());
-                break;
-        }
-
-        if ("desc".equalsIgnoreCase(sortOrder)) {
-            Comparator<HogwartsStudent> finalComparator = comparator;
-            comparator = (a, b) -> finalComparator.compare(b, a);
-        }
-
-        return comparator;
-    }
-    public Comparator<HogwartsTeacher> chooseTeacherComparator(String sortField, String sortOrder) {
-        Comparator<HogwartsTeacher> comparator = (a, b) -> 0; // Ingen sortering
-
-        switch(sortField.toLowerCase()) {
-            case "firstname":
-                comparator = (a, b) -> a.getFirstName().compareTo(b.getFirstName());
-                break;
-            case "lastname":
-                comparator = (a, b) -> a.getLastName().compareTo(b.getLastName());
-                break;
-            case "house":
-                comparator = (a, b) -> {
-                    // Brugt ternary fordi Dumbledore ikke er knyttet et hus
-                    String houseNameA = a.getHouse() != null ? a.getHouse().getName() : "";
-                    String houseNameB = b.getHouse() != null ? b.getHouse().getName() : "";
-                    return houseNameA.compareTo(houseNameB);
-                };
-                break;
-        }
-
-        if ("desc".equalsIgnoreCase(sortOrder)) {
-            Comparator<HogwartsTeacher> finalComparator = comparator;
-            comparator = (a, b) -> finalComparator.compare(b, a);
-        }
-
-        return comparator;
-    }
-
-
     private void sortMenu() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Type 'firstname', 'lastname', or 'house' to sort:");
@@ -101,33 +49,46 @@ public class UserInterFace {
         System.out.println("Type 'asc' for ascending, or 'desc' for descending:");
         String sortOrder = scanner.nextLine();
 
-        Comparator<HogwartsStudent> studentComparator = chooseStudentComparator(sortField, sortOrder);
-        Comparator<HogwartsTeacher> teacherComparator = chooseTeacherComparator(sortField, sortOrder);
-        List<HogwartsStudent> sortedStudents = studentController.getAllSorted(studentComparator);
-        List<HogwartsTeacher> sortedTeachers = teacherController.getAllSorted(teacherComparator);
+        List<HogwartsStudent> sortedStudents;
+        List<HogwartsTeacher> sortedTeachers;
 
-        printStudents(sortedStudents);
-        printTeachers(sortedTeachers);
-    }
-
-    private void printTeachers(List<HogwartsTeacher> sortedTeachers) {
-        String headerFormat = "| %-12s | %-12s | %-12s | %-12s |\n";
-        System.out.format(headerFormat, "First Name", "Last Name", "House", "Role");
-        System.out.println(new String(new char[50]).replace("\0", "-"));
-
-        for (HogwartsTeacher teacher : sortedTeachers) {
-            String rowFormat = "| %-12s | %-12s | %-12s | %-12s |\n";
-            // Ternary bruges her, da nogle lærere måske ikke er knyttet til et hus
-            String houseName = teacher.getHouse() != null ? teacher.getHouse().getName() : "None";
-            System.out.format(rowFormat,
-                    teacher.getFirstName(),
-                    teacher.getLastName(),
-                    houseName,
-                    "Teacher");
+        switch(sortField.toLowerCase()) {
+            case "firstname":
+                if ("asc".equalsIgnoreCase(sortOrder)) {
+                    sortedStudents = studentController.getAllSorted((a, b) -> a.getFirstName().compareTo(b.getFirstName()));
+                    sortedTeachers = teacherController.getAllSorted((a, b) -> a.getFirstName().compareTo(b.getFirstName()));
+                } else {
+                    sortedStudents = studentController.getAllSorted((a, b) -> b.getFirstName().compareTo(a.getFirstName()));
+                    sortedTeachers = teacherController.getAllSorted((a, b) -> b.getFirstName().compareTo(a.getFirstName()));
+                }
+                break;
+            case "lastname":
+                if ("asc".equalsIgnoreCase(sortOrder)) {
+                    sortedStudents = studentController.getAllSorted((a, b) -> a.getLastName().compareTo(b.getLastName()));
+                    sortedTeachers = teacherController.getAllSorted((a, b) -> a.getLastName().compareTo(b.getLastName()));
+                } else {
+                    sortedStudents = studentController.getAllSorted((a, b) -> b.getLastName().compareTo(a.getLastName()));
+                    sortedTeachers = teacherController.getAllSorted((a, b) -> b.getLastName().compareTo(a.getLastName()));
+                }
+                break;
+            case "house":
+                if ("asc".equalsIgnoreCase(sortOrder)) {
+                    sortedStudents = studentController.getAllSorted((a, b) -> a.getHouse().getName().compareTo(b.getHouse().getName()));
+                    sortedTeachers = teacherController.getAllSorted((a, b) -> a.getHouse().getName().compareTo(b.getHouse().getName()));
+                } else {
+                    sortedStudents = studentController.getAllSorted((a, b) -> b.getHouse().getName().compareTo(a.getHouse().getName()));
+                    sortedTeachers = teacherController.getAllSorted((a, b) -> b.getHouse().getName().compareTo(a.getHouse().getName()));
+                }
+                break;
+            default:
+                System.out.println("Invalid sort field");
+                return;
         }
+
+            printSorted(sortedStudents, sortedTeachers);
     }
 
-    private void printStudents(List<HogwartsStudent> sortedStudents) {
+    private void printSorted(List<HogwartsStudent> sortedStudents, List<HogwartsTeacher> sortedTeachers) {
         String headerFormat = "| %-12s | %-12s | %-12s | %-12s |\n";
         System.out.format(headerFormat, "First Name", "Last Name", "House", "Role");
         System.out.println(new String(new char[50]).replace("\0", "-"));
@@ -140,5 +101,16 @@ public class UserInterFace {
                     student.getHouse().getName(), // Antager at getHouse() returnerer et objekt, der har en getName() metode.
                     "Student");
         }
+        for (HogwartsTeacher teacher : sortedTeachers) {
+            String rowFormat = "| %-12s | %-12s | %-12s | %-12s |\n";
+            // Ternary bruges her, da nogle lærere måske ikke er knyttet til et hus
+            String houseName = teacher.getHouse() != null ? teacher.getHouse().getName() : "None";
+            System.out.format(rowFormat,
+                    teacher.getFirstName(),
+                    teacher.getLastName(),
+                    houseName,
+                    "Teacher");
+        }
     }
+
 }
